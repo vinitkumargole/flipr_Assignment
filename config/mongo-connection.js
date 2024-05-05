6d964f0faf6bb637ff26dd5547fe7d99d242ece3
vinitkumargole <77961291+vinitkumargole@users.noreply.github.com>
2024-05-06 00:27:17 +0530

    db url set

--- config/mongo-connection.js
+++ config/mongo-connection.js
@@ -4,7 +4,7 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url='mongodb://127.0.0.1:27017'
+    const url='mongodb+srv://vinitkumar20049:ppgL53MsS869YkLj@flipr.a2idthg.mongodb.net/'
     const dbname='flipr'
 
     mongoClient.connect(url,(err,data)=>{
